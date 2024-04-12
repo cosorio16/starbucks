bba7baf75a928582c366bae5c7650051592eb893
cosorio16 <carlososorio1230@gmail.com>
2024-04-12 14:39:49 -0500

    Starbucks web apage clone with astro.js and tailwindCSS, index page complete

---
+++ src/heroData.js
@@ -0,0 +1,43 @@
+const heroData = [
+  {
+    image: "/cookies-and-cream.jpeg",
+    description:
+      "¡Cookies and Cream Frappuccino, Chocolate Cookies and Cream Frappuccino!",
+    title: "Cookies and Cream ",
+    subtitle: "Regresa",
+    button: "",
+    link: "",
+    color: "#75466f",
+    background: "#d5d0e4",
+  },
+  {
+    image: "/viajemos.jpeg",
+    description: "",
+    subtitle: "Viaja por Colombia ",
+    button: "Conoce mas",
+    link: "https://www.starbucks.com.co/seccion/nuestro-cafe",
+    background: "#f2f0eb",
+    color: "#201f1f",
+  },
+  {
+    image: "/nuestro_planeta.jpg",
+    description:
+      "Juntos seguimos construyendo un futuro más sostenible para el café 💚",
+    subtitle: "INFORME DE SUSTENTABILIDAD",
+    button: "Leer Informe",
+    link: "https://historias.starbucks.com/es/stories/2020/informe-2019-sostenibilidad/",
+    color: "#ffffff",
+    background: "#1e3932",
+  },
+  {
+    image: "/tyc.jpg",
+    description: "Términos y Condiciones ACTIVIDADES",
+    title: "",
+    button: "Mas informacion",
+    link: "https://www.starbucks.com.co/seccion/terminos-condiciones",
+    background: "#1e3932",
+    color: "#ffffff",
+  },
+];
+
+export default heroData;
