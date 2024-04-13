97af7a023e66029321551fe10cae89092c36f39f
cosorio16 <carlososorio1230@gmail.com>
2024-04-13 18:47:41 -0500

    page/menu.astro complete

---
+++ src/data/menuData.js
@@ -0,0 +1,92 @@
+const menu = [
+  {
+    title: "Temporada Spring",
+    products: [
+      {
+        name: "Bebidas Spring",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2024-04%2FCHOCOLATE_COOKIES_%26_CREAM_FRAPPUCCINO_1.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+
+      {
+        name: "Alimentos Spring",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2024-04%2FPANINI_PAVO__0.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+    ],
+  },
+
+  {
+    title: "Bebidas",
+    products: [
+      {
+        name: "Café Caliente",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FLATTE-09_0.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop ",
+      },
+      {
+        name: "Bebidas Heladas",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FDOLCE-CANELA-LATTE-HELADO-VIDRIO.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+      {
+        name: "Frappuccinos",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FAREQUIPE%20FRAPPUCCINO.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+      {
+        name: "Refreshers",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FMANGO%20DRAGONFRUIT%20REFRESHER.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+      {
+        name: "Té",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FGREEN-TEA-LATTE-HOT-VIDRIO.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+      {
+        name: "Chocolate",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FCHOCOLATE_0.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+    ],
+  },
+  {
+    title: "Alimentos",
+    products: [
+      {
+        name: "Pastelería",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2023-03%2FMUFFIN-CHOCOLATE-WEB.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop ",
+      },
+      {
+        name: "Panaderia",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FCROISSANT-MANTEQUILLA-160X160%20(1)_0.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop ",
+      },
+      {
+        name: "Paninis",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2023-03%2FPANINI-MILANO.png?auto=format,compress&amp;q=70&amp;crop=focalpoint&amp;ar=1:1.0&amp;w=180&amp;fit=crop ",
+      },
+      {
+        name: "Tortas",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2023-03%2FTORTA-ZANAHORIA%20(1).png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+      {
+        name: "Comida Empacada",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2024-04%2F399F5E36-937B-49A8-90C1-BED59BC8BB3F-removebg-preview%20(1).png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+    ],
+  },
+  {
+    title: "Bebidas listas para llevar",
+    products: [
+      {
+        name: "Para llevar",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-02%2FJUGO_NARANAJA_0_1.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+    ],
+  },
+
+  {
+    title: "Café en grano",
+    products: [
+      {
+        name: "Nuestro Café",
+        img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2024-04%2FCOLOMBIA_MEDIO_MEDIA_LIBRA_1.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop",
+      },
+    ],
+  },
+];
+
+export default menu;
