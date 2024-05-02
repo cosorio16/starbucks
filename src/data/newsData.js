d505f0928fae7a6aa9e7a3b9449a2595ae907b5c
cosorio16 <carlososorio1230@gmail.com>
2024-05-02 17:17:12 -0500

    news sections novedades complete with new 2 components

---
+++ src/data/newsData.js
@@ -0,0 +1,47 @@
+const news = [
+  {
+    img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2024-01%2FGreen-store-global%20(1).jpg?auto=format,compress&q=70&crop=focalpoint&ar=1.85:1.0&w=530&fit=max",
+    link: "https://www.starbucks.com.co/articulo/starbucks-amplia-su-programa-de-greener-stores-global-anuncia-prim",
+    text: "Starbucks amplía su programa de Greener Stores global; anuncia primeras once Greener Store certificadas en Colombia",
+    date: "Enero 15, 2024",
+  },
+
+  {
+    img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2024-01%2FQuinta-tienda-en-cartagena%20(1).jpg?auto=format,compress&q=70&crop=focalpoint&ar=1.85:1.0&w=530&fit=max",
+    link: "https://www.starbucks.com.co/articulo/starbucks-abre-su-quinta-tienda-en-cartagena-en-la-historica-calle-bad",
+    text: "Starbucks abre su quinta tienda en Cartagena en la Histórica Calle Badillo",
+    date: "Enero 15, 2024",
+  },
+  {
+    img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-02%2FStarbucks_1.png?auto=format,compress&q=70&crop=focalpoint&ar=1.85:1.0&w=530&fit=max",
+    link: "https://www.starbucks.com.co/articulo/starbucks-50-anos-compartiendo-la-pasion-por-el-cafe-colombiano",
+    text: "Starbucks: 50 años compartiendo la pasión por el café colombiano",
+    date: "Enero 15, 2024",
+  },
+  {
+    img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2024-01%2F50-tiendas-en-colombia%20(1)_0.jpg?auto=format,compress&q=70&crop=focalpoint&ar=1.85:1.0&w=530&fit=max",
+    link: "https://www.starbucks.com.co/articulo/starbucks-supera-las-50-tiendas-en-colombia-y-abre-su-primer-drive-thr",
+    text: "Starbucks supera las 50 tiendas en Colombia y abre su primer drive-thru del país  ",
+    date: "enero 15, 2024",
+  },
+  {
+    img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2023-06%2FStarbucks%20juntos%20por%20el%20caf%C3%A9.jpg?auto=format,compress&q=70&crop=focalpoint&ar=1.85:1.0&w=530&fit=max",
+    link: "",
+    text: "Starbucks Colombia lanza el programa “Juntos por el Café” para apoyar a los caficultores locales",
+    date: "junio 27, 2023",
+  },
+  {
+    img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-02%2FMall-Plaza-2%20(1)_1.jpeg?auto=format,compress&q=70&crop=focalpoint&ar=1.85:1.0&w=530&fit=max",
+    link: "",
+    text: "Starbucks abre dos nuevas tiendas en Barranquilla",
+    date: "febrero 16, 2022",
+  },
+  {
+    img: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2Fstarbucks%20santander%20(3).jpeg?auto=format,compress&q=70&crop=focalpoint&ar=1.85:1.0&w=530&fit=max",
+    link: "",
+    text: "De las mujeres santandereanas para el mundo",
+    date: "febrero 16, 2022",
+  },
+];
+
+export default news;
